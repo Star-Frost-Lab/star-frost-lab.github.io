3c41f3f5e79bcf037f8e9beb4208beeae7a2bb0b
星霜笔记 <1613589162@qq.com>
2026-01-29 16:00:53 +0800

    更新 config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ export const config = {
 
   // 站点说明区域开关 + 内容
   siteInfo: {
-    enabled: false,  // ← 改成 false 即可完全关闭说明区域（不占位）
+    enabled: true,  // ← 改成 false 即可完全关闭说明区域（不占位）
     title: "关于这个页面",
     paragraphs: [
       "存放一些小项目，大部分是工具、实验性质的玩具，或者半夜突发奇想搞出来的东西。"
